e3ac908aafc779ad8253236a7fa88dbf1b266f01
Gabe Morales <gabomorales@me.com>
2017-03-14 17:16:46 -0500

    Added topMostViewController var

--- GMDKit/Classes/UIApplication+GMDKit.swift
+++ GMDKit/Classes/UIApplication+GMDKit.swift
@@ -6,4 +6,51 @@
 //
 //
 
-import Foundation
+import UIKit
+
+public extension UIApplication {
+    /**
+     The window used for presenting the root level of the user interface.
+     */
+    var mainWindow: UIWindow? {
+        guard let window = delegate?.window else {
+            print("Error: No main window!")
+            return nil
+        }
+        
+        guard let windowToReturn = window else {
+            print("Error: No main window!")
+            return nil
+        }
+        
+        return windowToReturn
+    }
+    
+    /**
+     The current top-most view controller, suitable for presenting an alert controller from.
+     */
+    var topmostViewController: UIViewController? {
+        guard let rootViewController = mainWindow?.rootViewController else {
+            return nil
+        }
+        
+        return topmostViewControllerWithRootViewController(rootViewController)
+    }
+    
+    private func topmostViewControllerWithRootViewController(_ rootViewController: UIViewController) -> UIViewController {
+        if let presented = rootViewController.presentedViewController {
+            return topmostViewControllerWithRootViewController(presented)
+        }
+        else if let navigationController = rootViewController as? UINavigationController,
+            let visibleViewController = navigationController.visibleViewController {
+            return topmostViewControllerWithRootViewController(visibleViewController)
+        }
+        else if let lastChild = rootViewController.childViewControllers.last {
+            return lastChild
+        }
+        else {
+            return rootViewController
+        }
+    }
+}
+
